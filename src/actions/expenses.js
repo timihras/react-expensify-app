3304050acf0e2a331e990fa60694bdf190359698
timihras <timi.hras@gmail.com>
2018-04-27 07:08:54 +0200

    Setup the fetch expenses feature

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -39,3 +39,43 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+
+    return database.ref('expenses')
+      .once('value')
+      .then((snapshot) => {
+
+        let expenses = []
+        snapshot.forEach((childSnapshot) => {
+          expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+          })
+        });
+
+        dispatch(setExpenses(expenses));
+      })
+  };
+};
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+
+//     console.log(expenses);
+//   });
--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from './selectors/expenses';
 
@@ -21,4 +21,9 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
+
